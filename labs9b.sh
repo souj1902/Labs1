ec5b7eeb09df96a21c6f81a60047664ee7b371a2
souj1902 <35593878+souj1902@users.noreply.github.com>
2018-03-24 12:14:03 +0530

    Add files via upload

---
+++ labs9b.sh
@@ -0,0 +1,7 @@
+echo "Enter word or phrase to be searched"
+read n
+for i in /
+do
+		find / "$n"
+
+	done
---
+++ souj9.sh
@@ -0,0 +1,6 @@
+names="Sne Swathi Deeps Dipali"
+PS3='Select names'
+select qwe in  $names
+do
+	echo hello $qwe
+done 
